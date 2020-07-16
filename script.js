02454e0316f0174c0997f0fdd59eaa21e7977d5e
DANIEL LANDES <daniellandes@DANIELs-MacBook-Air.local>
2020-07-16 15:10:51 +0100

    Updated indo url

--- script.js
+++ script.js
@@ -20,8 +20,17 @@ $(document).ready(function(){
   new WOW().init();
 
   const onload = () => {
+
+    setTimeout(function(){ 
+      document.querySelector('.card1').classList.remove('d-none');
+      document.querySelector('.card2').classList.remove('d-none');
+
+      document.querySelector('.card1').classList.add('slideInLeft','animated');
+      document.querySelector('.card2').classList.add('slideInRight','animated');
+     }, 1000);
+
     setTimeout(function(){ 
       document.querySelector('.profilepic').classList.add('animated','flip');
-     }, 1500);
+     }, 2500);
     
   }
